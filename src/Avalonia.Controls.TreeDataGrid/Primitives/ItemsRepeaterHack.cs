59eae85554606ce79dd93b2c2f29886d653b083a
Steven Kirk <grokys@gmail.com>
2020-12-15 16:00:45 +0100

    Added hack to speed things up.

---
+++ src/Avalonia.Controls.TreeDataGrid/Primitives/ItemsRepeaterHack.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia.Layout;
+
+namespace Avalonia.Controls.Primitives
+{
+    public class ItemsRepeaterHack : ItemsRepeater, ILayoutable
+    {
+        void ILayoutable.ChildDesiredSizeChanged(ILayoutable control)
+        {
+            // HACK: Don't invalidate measure if the child whose size changed is in the recycle
+            // pool. This needs to be implemented on ItemsRepeater itself.
+            if (GetElementIndex((IControl)control) >= 0)
+                InvalidateMeasure();
+        }
+    }
+}
