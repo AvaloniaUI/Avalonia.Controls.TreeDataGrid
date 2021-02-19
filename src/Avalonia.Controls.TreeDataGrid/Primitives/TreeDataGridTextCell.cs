751981e31852cd4d9930ce848690ea9e521c8451
Steven Kirk <grokys@gmail.com>
2021-02-19 12:37:59 +0100

    Make text cells display changes to the cell value.

--- src/Avalonia.Controls.TreeDataGrid/Primitives/TreeDataGridTextCell.cs
+++ src/Avalonia.Controls.TreeDataGrid/Primitives/TreeDataGridTextCell.cs
@@ -1,21 +1,6 @@
-﻿using System;
-using Avalonia.Controls.Models.TreeDataGrid;
-
-namespace Avalonia.Controls.Primitives
+﻿namespace Avalonia.Controls.Primitives
 {
     public class TreeDataGridTextCell : TreeDataGridCell
     {
-        public static readonly DirectProperty<TreeDataGridTextCell, string?> ValueProperty =
-            AvaloniaProperty.RegisterDirect<TreeDataGridTextCell, string?>(
-                nameof(Value),
-                o => o.Value);
-
-        public string? Value => ((ICell?)DataContext)?.Value?.ToString();
-
-        protected override void OnDataContextChanged(EventArgs e)
-        {
-            base.OnDataContextChanged(e);
-            RaisePropertyChanged(ValueProperty, default, default);
-        }
     }
 }
