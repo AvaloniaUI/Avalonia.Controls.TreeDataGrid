1cc4225ded9334b316a9600c3c204a16f6e3fa2a
Steven Kirk <grokys@gmail.com>
2021-08-30 23:35:34 +0200

    WIP

--- tests/Avalonia.Controls.TreeDataGrid.Tests/Selection/TreeSelectionModelBaseTests_Single.cs
+++ tests/Avalonia.Controls.TreeDataGrid.Tests/Selection/TreeSelectionModelBaseTests_Single.cs
@@ -191,6 +191,72 @@ namespace Avalonia.Controls.TreeDataGridTests
             }
         }
 
+        public class SelectedItem
+        {
+            [Fact]
+            public void PropertyChanged_Is_Raised_When_SelectedIndex_Changes()
+            {
+                var target = CreateTarget();
+                var raised = 0;
+
+                target.PropertyChanged += (s, e) =>
+                {
+                    if (e.PropertyName == nameof(target.SelectedItem))
+                    {
+                        ++raised;
+                    }
+                };
+
+                target.SelectedIndex = new IndexPath(1);
+
+                Assert.Equal(1, raised);
+            }
+        }
+
+        public class SelectedIndexes
+        {
+            [Fact]
+            public void PropertyChanged_Is_Raised_When_SelectedIndex_Changes()
+            {
+                var target = CreateTarget();
+                var raised = 0;
+
+                target.PropertyChanged += (s, e) =>
+                {
+                    if (e.PropertyName == nameof(target.SelectedIndexes))
+                    {
+                        ++raised;
+                    }
+                };
+
+                target.SelectedIndex = new IndexPath(1);
+
+                Assert.Equal(1, raised);
+            }
+        }
+
+        public class SelectedItems
+        {
+            [Fact]
+            public void PropertyChanged_Is_Raised_When_SelectedIndex_Changes()
+            {
+                var target = CreateTarget();
+                var raised = 0;
+
+                target.PropertyChanged += (s, e) =>
+                {
+                    if (e.PropertyName == nameof(target.SelectedItems))
+                    {
+                        ++raised;
+                    }
+                };
+
+                target.SelectedIndex = new IndexPath(1);
+
+                Assert.Equal(1, raised);
+            }
+        }
+        
         public class Select
         {
             [Fact]
@@ -273,6 +339,224 @@ namespace Avalonia.Controls.TreeDataGridTests
                 target.Select(new IndexPath(0, 2));
 
                 Assert.Equal(0, raised);
+            }
+        }
+
+        public class Deselect
+        {
+            [Fact]
+            public void Deselect_Clears_Current_Selection()
+            {
+                var target = CreateTarget();
+                var raised = 0;
+
+                target.SelectedIndex = new IndexPath(0, 1);
+
+                target.SelectionChanged += (s, e) =>
+                {
+                    Assert.Equal(new[] { new IndexPath(0, 1) }, e.DeselectedIndexes);
+                    Assert.Equal(new[] { "Node 0-1" }, e.DeselectedItems.Select(x => x.Caption));
+                    Assert.Empty(e.SelectedIndexes);
+                    Assert.Empty(e.SelectedItems);
+                    ++raised;
+                };
+
+                target.Deselect(new IndexPath(0, 1));
+
+                Assert.Equal(default, target.SelectedIndex);
+                Assert.Empty(target.SelectedIndexes);
+                Assert.Null(target.SelectedItem);
+                Assert.Empty(target.SelectedItems);
+                Assert.Equal(1, raised);
+            }
+
+            [Fact]
+            public void Deselect_Does_Nothing_For_Nonselected_Item()
+            {
+                var target = CreateTarget();
+                var raised = 0;
+
+                target.SelectedIndex = new IndexPath(0, 1);
+                target.SelectionChanged += (s, e) => ++raised;
+                target.Deselect(new IndexPath(0, 0));
+
+                Assert.Equal(new IndexPath(0, 1), target.SelectedIndex);
+                Assert.Equal(new[] { new IndexPath(0, 1) }, target.SelectedIndexes);
+                Assert.Equal("Node 0-1", target.SelectedItem!.Caption);
+                Assert.Equal(new[] { "Node 0-1" }, target.SelectedItems.Select(x => x.Caption));
+                Assert.Equal(0, raised);
+            }
+        }
+
+        public class Clear
+        {
+            [Fact]
+            public void Clear_Raises_SelectionChanged()
+            {
+                var target = CreateTarget();
+                var raised = 0;
+
+                target.Select(new IndexPath(0, 1));
+
+                target.SelectionChanged += (s, e) =>
+                {
+                    Assert.Equal(new[] { new IndexPath(0, 1) }, e.DeselectedIndexes);
+                    Assert.Equal(new[] { "Node 0-1" }, e.DeselectedItems.Select(x => x.Caption));
+                    Assert.Empty(e.SelectedIndexes);
+                    Assert.Empty(e.SelectedItems);
+                    ++raised;
+                };
+
+                target.Clear();
+
+                Assert.Equal(1, raised);
+            }
+        }
+
+        public class AnchorIndex
+        {
+            [Fact]
+            public void Setting_SelectedIndex_Sets_AnchorIndex()
+            {
+                var target = CreateTarget();
+                var raised = 0;
+
+                target.PropertyChanged += (s, e) =>
+                {
+                    if (e.PropertyName == nameof(target.AnchorIndex))
+                    {
+                        ++raised;
+                    }
+                };
+
+                target.SelectedIndex = new IndexPath(0, 1);
+
+                Assert.Equal(new IndexPath(0, 1), target.AnchorIndex);
+                Assert.Equal(1, raised);
+            }
+
+            [Fact]
+            public void Setting_SelectedIndex_To_Empty_Doesnt_Clear_AnchorIndex()
+            {
+                var target = CreateTarget();
+                var raised = 0;
+
+                target.SelectedIndex = new IndexPath(0, 1);
+
+                target.PropertyChanged += (s, e) =>
+                {
+                    if (e.PropertyName == nameof(target.AnchorIndex))
+                    {
+                        ++raised;
+                    }
+                };
+
+                target.SelectedIndex = default;
+
+                Assert.Equal(new IndexPath(0, 1), target.AnchorIndex);
+                Assert.Equal(0, raised);
+            }
+
+            [Fact]
+            public void Select_Sets_AnchorIndex()
+            {
+                var target = CreateTarget();
+                var raised = 0;
+
+                target.PropertyChanged += (s, e) =>
+                {
+                    if (e.PropertyName == nameof(target.AnchorIndex))
+                    {
+                        ++raised;
+                    }
+                };
+
+                target.Select(new IndexPath(0, 1));
+
+                Assert.Equal(new IndexPath(0, 1), target.AnchorIndex);
+                Assert.Equal(1, raised);
+            }
+
+            [Fact]
+            public void Deselect_Doesnt_Clear_AnchorIndex()
+            {
+                var target = CreateTarget();
+                var raised = 0;
+
+                target.Select(new IndexPath(0, 1));
+
+                target.PropertyChanged += (s, e) =>
+                {
+                    if (e.PropertyName == nameof(target.AnchorIndex))
+                    {
+                        ++raised;
+                    }
+                };
+
+                target.Deselect(new IndexPath(0, 1));
+
+                Assert.Equal(new IndexPath(0, 1), target.AnchorIndex);
+                Assert.Equal(0, raised);
+            }
+
+            [Fact]
+            public void Raises_PropertyChanged()
+            {
+                var target = CreateTarget();
+                var raised = 0;
+
+                target.PropertyChanged += (s, e) =>
+                {
+                    if (e.PropertyName == nameof(target.AnchorIndex))
+                    {
+                        ++raised;
+                    }
+                };
+
+                target.SelectedIndex = new IndexPath(0, 1);
+
+                Assert.Equal(1, raised);
+            }
+        }
+
+        public class SingleSelect
+        {
+            [Fact]
+            public void Converting_To_Multiple_Selection_Preserves_Selection()
+            {
+                var target = CreateTarget();
+                var raised = 0;
+
+                target.SelectedIndex = new IndexPath(0, 1);
+
+                target.SelectionChanged += (s, e) => ++raised;
+
+                target.SingleSelect = false;
+
+                Assert.Equal(new IndexPath(0, 1), target.SelectedIndex);
+                Assert.Equal(new[] { new IndexPath(0, 1) }, target.SelectedIndexes);
+                Assert.Equal("Node 0-1", target.SelectedItem!.Caption);
+                Assert.Equal(new[] { "Node 0-1" }, target.SelectedItems.Select(x => x.Caption));
+                Assert.Equal(0, raised);
+            }
+
+            [Fact]
+            public void Raises_PropertyChanged()
+            {
+                var target = CreateTarget();
+                var raised = 0;
+
+                target.PropertyChanged += (s, e) =>
+                {
+                    if (e.PropertyName == nameof(target.SingleSelect))
+                    {
+                        ++raised;
+                    }
+                };
+
+                target.SingleSelect = false;
+
+                Assert.Equal(1, raised);
             }
         }
 
