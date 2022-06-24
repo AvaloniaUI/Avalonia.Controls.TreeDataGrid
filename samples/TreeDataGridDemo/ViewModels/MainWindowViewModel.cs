e022f0cad87d6a73795cf58d5d88174697ce7d26
Steven Kirk <grokys@gmail.com>
2022-06-24 09:18:02 +0200

    Remove test code.

--- samples/TreeDataGridDemo/ViewModels/MainWindowViewModel.cs
+++ samples/TreeDataGridDemo/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections.ObjectModel;
-using Avalonia.Controls;
-using Avalonia.Controls.Models.TreeDataGrid;
-
-namespace TreeDataGridDemo.ViewModels
+﻿namespace TreeDataGridDemo.ViewModels
 {
     internal class MainWindowViewModel
     {
@@ -18,41 +14,5 @@ namespace TreeDataGridDemo.ViewModels
         {
             get => _files ??= new FilesPageViewModel();
         }
-
-        public TestPageViewModel Test { get; } = new();
-    }
-
-    internal class TestPageViewModel
-    {
-        public TestPageViewModel()
-        {
-            Root = new TestNode { Name = "Root" };
-            Source = new HierarchicalTreeDataGridSource<TestNode>(Root)
-            {
-                Columns =
-                {
-                    new HierarchicalExpanderColumn<TestNode>(
-                        new TextColumn<TestNode, string>("Name", x => x.Name),
-                        x => x.Children),
-                },
-            };
-        }
-
-        public HierarchicalTreeDataGridSource<TestNode> Source { get; }
-        public TestNode Root { get; }
-
-        public void DoIt()
-        {
-            if (Root.Children.Count > 0)
-                Root.Children.RemoveAt(0);
-            else
-                Root.Children.Add(new TestNode { Name = "Child" });
-        }
-    }
-
-    internal class TestNode
-    {
-        public string? Name { get; set; }
-        public ObservableCollection<TestNode> Children { get; } = new();
     }
 }
