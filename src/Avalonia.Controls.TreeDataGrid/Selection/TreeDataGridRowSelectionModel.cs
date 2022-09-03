e7b8b3fe271050a0cbbd4e36bc8a955a2198160a
Steven Kirk <grokys@gmail.com>
2022-09-03 21:15:09 +0200

    Make selection happen on mouse up.
    Otherwise dragging a multiple selection isn't possible.

--- src/Avalonia.Controls.TreeDataGrid/Selection/TreeDataGridRowSelectionModel.cs
+++ src/Avalonia.Controls.TreeDataGrid/Selection/TreeDataGridRowSelectionModel.cs
@@ -307,10 +307,7 @@ namespace Avalonia.Controls.Selection
 
         void ITreeDataGridSelectionInteraction.OnPointerPressed(TreeDataGrid sender, PointerPressedEventArgs e)
         {
-            if (!e.Handled && e.Pointer.Type == PointerType.Mouse)
-                PointerSelect(sender, e);
-            else
-                _pressedPoint = e.GetPosition(sender);
+            _pressedPoint = e.GetPosition(sender);
         }
 
         void ITreeDataGridSelectionInteraction.OnTextInput(TreeDataGrid sender, TextInputEventArgs e)
@@ -320,7 +317,7 @@ namespace Avalonia.Controls.Selection
 
         void ITreeDataGridSelectionInteraction.OnPointerReleased(TreeDataGrid sender, PointerReleasedEventArgs e)
         {
-            if (!e.Handled && e.Pointer.Type == PointerType.Touch)
+            if (!e.Handled)
             {
                 var p = e.GetPosition(sender);
                 if (Math.Abs(p.X - _pressedPoint.X) <= 3 || Math.Abs(p.Y - _pressedPoint.Y) <= 3)
