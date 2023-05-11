62cef2215a8a3bf0eee367d50a81e46d02c1eb52
Steven Kirk <grokys@users.noreply.github.com>
2023-05-11 16:09:26 +0200

    Skeleton of cell selection model.

---
+++ src/Avalonia.Controls.TreeDataGrid/Selection/ITreeDataGridCellSelectionModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Avalonia.Controls.Models.TreeDataGrid;
+
+namespace Avalonia.Controls.Selection
+{
+    public interface ITreeDataGridCellSelectionModel : ITreeDataGridSelection
+    {
+    }
+
+    public interface ITreeDataGridCellSelectionModel<T> : ITreeDataGridCellSelectionModel
+        where T : class
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether only a single cell can be selected at a time.
+        /// </summary>
+        bool SingleSelect { get; set; }
+
+        /// <summary>
+        /// Gets the currently selected cells.
+        /// </summary>
+        IReadOnlyList<ICell> SelectedCells { get; }
+
+        /// <summary>
+        /// Gets the currently selected columns.
+        /// </summary>
+        ITreeDataGridColumnSelectionModel SelectedColumns { get; }
+
+        /// <summary>
+        /// Gets the currently selected rows.
+        /// </summary>
+        ITreeDataGridRowSelectionModel<T> SelectedRows { get; }
+    }
+}
---
+++ src/Avalonia.Controls.TreeDataGrid/Selection/TreeDataGridCellSelectionModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Avalonia.Controls.Models.TreeDataGrid;
+
+namespace Avalonia.Controls.Selection
+{
+    public class TreeDataGridCellSelectionModel<TModel> : ITreeDataGridCellSelectionModel<TModel>
+        where TModel : class
+    {
+        public TreeDataGridCellSelectionModel(ITreeDataGridSource<TModel> source)
+        {
+            SelectedCells = Array.Empty<ICell>();
+            SelectedColumns = new TreeDataGridColumnSelectionModel(source.Columns);
+            SelectedRows = new TreeDataGridRowSelectionModel<TModel>(source);
+        }
+
+        public bool SingleSelect
+        {
+            get => SelectedRows.SingleSelect;
+            set => SelectedColumns.SingleSelect = SelectedRows.SingleSelect = value;
+        }
+
+        public IReadOnlyList<ICell> SelectedCells { get; }
+        public ITreeDataGridColumnSelectionModel SelectedColumns { get; }
+        public ITreeDataGridRowSelectionModel<TModel> SelectedRows { get; }
+
+        IEnumerable? ITreeDataGridSelection.Source
+        {
+            get => ((ITreeDataGridSelection)SelectedRows).Source;
+            set => ((ITreeDataGridSelection)SelectedRows).Source = value;
+        }
+    }
+}
