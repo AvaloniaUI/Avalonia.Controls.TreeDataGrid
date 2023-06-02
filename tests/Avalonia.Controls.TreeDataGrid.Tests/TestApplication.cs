6c03e243bd1fece2860f616a5044b45afff8e8c5
Steven Kirk <grokys@users.noreply.github.com>
2023-06-02 15:25:10 +0200

    We don't need a xaml file for TestApplication.

--- tests/Avalonia.Controls.TreeDataGrid.Tests/TestApplication.cs
+++ tests/Avalonia.Controls.TreeDataGrid.Tests/TestApplication.cs
@@ -8,11 +8,6 @@ namespace Avalonia.Controls.TreeDataGridTests
 {
     public class TestApplication : Application
     {
-        public override void Initialize()
-        {
-            AvaloniaXamlLoader.Load(this);
-        }
-
         public static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<TestApplication>()
             .UseHeadless(new AvaloniaHeadlessPlatformOptions { UseHeadlessDrawing = true });
     }
