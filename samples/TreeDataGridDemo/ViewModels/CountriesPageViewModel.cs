3807564640ef53462f29e33a029abae16ce733a7
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2024-02-07 19:44:32 +0100

    Repro for issue 256

--- samples/TreeDataGridDemo/ViewModels/CountriesPageViewModel.cs
+++ samples/TreeDataGridDemo/ViewModels/CountriesPageViewModel.cs
@@ -21,14 +21,14 @@ namespace TreeDataGridDemo.ViewModels
             {
                 Columns =
                 {
-                    new TextColumn<Country, string>("Country", x => x.Name, (r, v) => r.Name = v, new GridLength(6, GridUnitType.Star), new()
+                    new TextColumn<Country, string>("Country", x => x.Name, (r, v) => r.Name = v, new GridLength(1, GridUnitType.Star), new()
                     {
                         IsTextSearchEnabled = true,
                     }),
                     new TemplateColumn<Country>("Region", "RegionCell", "RegionEditCell"),
-                    new TextColumn<Country, int>("Population", x => x.Population, new GridLength(3, GridUnitType.Star)),
-                    new TextColumn<Country, int>("Area", x => x.Area, new GridLength(3, GridUnitType.Star)),
-                    new TextColumn<Country, int>("GDP", x => x.GDP, new GridLength(3, GridUnitType.Star), new()
+                    new TextColumn<Country, int>("Population", x => x.Population, new GridLength(200, GridUnitType.Auto), new TextColumnOptions<Country>() { MinWidth = new GridLength(300)}),
+                    new TextColumn<Country, int>("Area", x => x.Area, new GridLength(200, GridUnitType.Auto), new TextColumnOptions<Country>() { MinWidth = new GridLength(200)}),
+                    new TextColumn<Country, int>("GDP", x => x.GDP, new GridLength(200, GridUnitType.Auto), new()
                     {
                         TextAlignment = Avalonia.Media.TextAlignment.Right,
                         MaxWidth = new GridLength(150)
