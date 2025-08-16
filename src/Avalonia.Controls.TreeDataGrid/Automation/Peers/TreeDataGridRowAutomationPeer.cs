cccc806fb790576ba6216589c9b2d46a6c819af2
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-08-16 13:56:25 +0200

    Implement IToggleProvider and IValueProvider for TreeDataGridRowAutomationPeer

--- src/Avalonia.Controls.TreeDataGrid/Automation/Peers/TreeDataGridRowAutomationPeer.cs
+++ src/Avalonia.Controls.TreeDataGrid/Automation/Peers/TreeDataGridRowAutomationPeer.cs
@@ -1,13 +1,40 @@
+using System;
 using Avalonia.Automation.Peers;
+using Avalonia.Automation.Provider;
+using Avalonia.Controls.Models.TreeDataGrid;
 using Avalonia.Controls.Primitives;
 
 namespace Avalonia.Controls.Automation.Peers;
 
-public class TreeDataGridRowAutomationPeer : ControlAutomationPeer
+public class TreeDataGridRowAutomationPeer : ControlAutomationPeer, IToggleProvider, IValueProvider
 {
     public TreeDataGridRowAutomationPeer(TreeDataGridRow owner)
         : base(owner)
     {
+    }
+
+    public new TreeDataGridRow Owner => (TreeDataGridRow)base.Owner;
+
+    public ToggleState ToggleState
+    {
+        get
+        {
+            if (Owner.DataContext is IExpander expander)
+            {
+                return expander.IsExpanded ? ToggleState.On : ToggleState.Off;
+            }
+            return ToggleState.Indeterminate;
+        }
+    }
+
+    public bool IsReadOnly => true;
+
+    public string? Value
+    {
+        get
+        {
+            return Owner.Model?.ToString();
+        }
     }
 
     protected override AutomationControlType GetAutomationControlTypeCore()
@@ -18,4 +45,17 @@ public class TreeDataGridRowAutomationPeer : ControlAutomationPeer
     protected override bool IsContentElementCore() => true;
 
     protected override bool IsControlElementCore() => true;
+
+    public void Toggle()
+    {
+        if (Owner.DataContext is IExpander expander)
+        {
+            expander.IsExpanded = !expander.IsExpanded;
+        }
+    }
+
+    public void SetValue(string? value)
+    {
+        throw new InvalidOperationException("TreeDataGrid rows are read-only.");
+    }
 }
